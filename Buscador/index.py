c8141c78bfc1442bd93564e190c0a610ef5c8d84
Darthzner <58405045+Darthzner@users.noreply.github.com>
2021-09-19 18:50:40 -0300

    Imagen redis con ubuntu

--- Buscador/index.py
+++ Buscador/index.py
@@ -1,9 +1,10 @@
 import redis
+import grpc, grpc_tools
 import json
 from flask import Flask,  jsonify
-from flask_restful import Api
 from werkzeug.exceptions import HTTPException
 from werkzeug.exceptions import default_exceptions
+
 
 app = Flask(__name__)
 
@@ -18,13 +19,25 @@ def handle_error(e):
 for ex in default_exceptions:
     app.register_error_handler(ex, handle_error)
 
-api = Api(app)
-api.prefix = '/api'
 
+@app.route('/api/getprod',methods=['GET'])
+def get_prods():
+    with open("../Inventario/inventario.json") as prods:
+        jsonObject = json.load(prods)
+        prods.close()
+        return jsonify(jsonObject['products_list'])
+    
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+""" 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 r.set("hello1",json.dumps({"nombre" : "dimeloMa"}))
 
 value = r.get('hello1')
 
-print(value)
+print(value) """
